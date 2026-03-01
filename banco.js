f3d03fae4ccf9f93328a5ffcbfe5d9a5483fe467
Goku <2204mariadejesus1949@gmail.com>
2026-02-28 21:00:29 -0300

    Add banco.js with initial conversation data

---
+++ banco.js
@@ -0,0 +1,5 @@
+export const banco = [
+  { palavras: ["oi", "opa"], respostas: ["Oi!", "Opa! Como vai?"] },
+  { palavras: ["tudo bem"], respostas: ["Estou bem, e você?", "Tudo ótimo!"] },
+  // ... 1000 objetos
+];
